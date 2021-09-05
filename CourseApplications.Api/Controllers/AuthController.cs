476545933c078fa13d5f3efe5679c2d5241afbd8
Freedom Khanyile <freedom.khanyile1@gmail.com>
2021-09-05 17:00:18 +0200

    Commit Project Created With Default Weather Controller and Swagger

---
+++ CourseApplications.Api/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseApplications.Api.Controllers
+{
+    [Route("")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+
+    }
+}
