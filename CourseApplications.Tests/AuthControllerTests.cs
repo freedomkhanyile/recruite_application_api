307023559d56b6154869bf646c6a58a692cb51e0
Freedom Khanyile <freedom.khanyile1@gmail.com>
2021-09-05 20:22:50 +0200

    Created integration tests for auth controller.

---
+++ CourseApplications.Tests/AuthControllerTests.cs
@@ -0,0 +1,46 @@
+﻿using CourseApplications.Api;
+using CourseApplications.Api.Models.Users;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CourseApplications.Tests
+{
+    [Collection("Integration Tests")]
+    public class AuthControllerTests
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public AuthControllerTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task Get_ReturnsAllUsers()
+        {
+            // Arrange
+
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("/Auth/users");
+
+            // Assert
+            response.EnsureSuccessStatusCode();
+            Assert.NotNull(response.Content);
+
+            var responseObj = JsonSerializer
+                .Deserialize<List<UserModel>>(
+                await response.Content.ReadAsStringAsync(), new JsonSerializerOptions { PropertyNameCaseInsensitive = true, });
+
+            Assert.Equal(3, responseObj.Count);
+
+        }
+    }
+}
---
+++ CourseApplications.Tests/TestCollection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CourseApplications.Tests
+{
+    [CollectionDefinition("Integration Tests")]
+    // Bellow WebApplicationFactory is using a Startup class. That is the same Startup class used by the main program
+    // So all of our service bindings and configuration will be part of our TestHost.
+    public class TestCollection: ICollectionFixture<WebApplicationFactory<CourseApplications.Api.Startup>>
+    {
+        // This class has no code, and is never created. Its purpose is simply
+        // to be the place to apply [CollectionDefinition] and all the
+        // ICollectionFixture<> interfaces.
+    }
+}
